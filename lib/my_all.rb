a34022c581bba55407de502a5b659e509b5465b9
Victor <victorrivera714@yahoo.com>
2020-10-29 18:17:28 -0700

    commit tree 86d7939082118a1cfb64c20057ca322695ba3586

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
+    i = 0
+    block_return_values= [] #array to save return value of the yield block
+    while i < collection.length
+        block_return_values << yield(collection[i])
+        i= i + 1 #i+= does the same thing.Use this if its easier for you. 
+    end
+    if block_return_values.include?(false)
+        false
+        else
+        true
+
+
+    end
 
 end
